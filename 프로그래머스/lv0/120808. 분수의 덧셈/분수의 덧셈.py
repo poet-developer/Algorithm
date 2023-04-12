76e736a50b06d4c83be22dd56a7767e107bfd4dc
poetdeveloper <90435185+poet-developer@users.noreply.github.com>
2023-04-12 22:54:56 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,22 +1,10 @@
+import math
+
 def solution(numer1, denom1, numer2, denom2):
     y = denom1 * denom2
-    x1 = numer1*denom2
-    x2 = numer2*denom1
-    x3 = x1+x2
+    x = (numer1*denom2)+(numer2*denom1)
     count = 1
-    same = [1]
-    answer = []
     
-    if(x3 >= y) :
-        j = y
-    else :
-        j = x3
-    
-    for i in range(1,j+1) :
-        if x3%count == 0 and y%count == 0 :
-            same.append(count)
-        count = count + 1
-    answer.append(x3/max(same))
-    answer.append(y/max(same))
+    math.gcd(x, y)
 
-    return answer
+    return [x//math.gcd(x, y), y//math.gcd(x, y)]
