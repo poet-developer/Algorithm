02fc74b3680539589d03c22bb9927b71a1999f99
poetdeveloper <90435185+poet-developer@users.noreply.github.com>
2023-04-16 14:35:21 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.py
+++ 프로그래머스/lv0/120829. 각도기/각도기.py
@@ -1,9 +1,10 @@
 def solution(angle):
+    answer = 4
     if 0 < angle and angle < 90 :
         return 1
     elif 90 == angle : 
         return 2
     elif 90 < angle and angle < 180 :
         return 3
-    elif angle == 180 :
-        return 4
+    else :
+        return answer
